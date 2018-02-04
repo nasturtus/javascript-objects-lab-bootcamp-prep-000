081dc6332375fb3a337b897c8ec27bdd203b981c
Santosh SN <nasturtus@gmail.com>
2018-02-04 11:27:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var recipes = {
 };
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
+  var newObject = Object.assign({}, object);
+  newObject[key] = value;
   return object;
 }
 
