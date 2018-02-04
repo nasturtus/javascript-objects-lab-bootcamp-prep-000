24f306cdb12e313b60a934dd4f4516cb7318f5b4
Santosh SN <nasturtus@gmail.com>
2018-02-04 11:25:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function deleteFromObjectByKey(object, key, value) {
+  var newObject = Object.assign({}, object);
+  delete newObject[key];
+  return newObject
+}
