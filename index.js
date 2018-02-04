590b8accfe920db36fc7cbce7c56e57eadd8f1fa
Santosh SN <nasturtus@gmail.com>
2018-02-04 11:29:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var recipes = {
 function updateObjectWithKeyAndValue(object, key, value) {
   var newObject = Object.assign({}, object);
   newObject[key] = value;
-  return object;
+  return newObject;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
